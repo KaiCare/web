7fa725a64e5df9321f9a1853581e626a1631e962
Laura Serrano Velázquez <lserranovelazquez@hawk.iit.edu>
2024-04-06 17:39:09 -0500

    withChat

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,35 @@
-import './globals.css';
-import Navbar from './components/Navbar/index';
-import Footer from './components/Footer/Footer';
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 
+import { ToasterProvider } from '@/components/toaster-provider'
+import { ModalProvider } from '@/components/modal-provider'
+import { CrispProvider } from '@/components/crisp-provider'
 
-export const metadata = {
-  title: 'KAICare',
-  description: 'KAIcare: Where AI Meets Muscles',
+import './globals.css'
+
+const font = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Genius',
+  description: 'AI Platform',
 }
 
-
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        
+        <body className={font.className}>
+          <ToasterProvider />
+          <ModalProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
---
+++ constants.ts
@@ -0,0 +1,20 @@
+import { Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+
+export const MAX_FREE_COUNTS = 5;
+
+export const tools = [
+  {
+    label: 'Conversation',
+    icon: MessageSquare,
+    href: '/conversation',
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+  },
+  {
+    label: 'Video Generation',
+    icon: VideoIcon,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: '/video',
+  },
+];
